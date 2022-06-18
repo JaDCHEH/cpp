bb49e53ec6a821e01788837eea59f79ae6f106ad
Jad Chehboune <cjad@e1r4p5.1337.ma>
2022-06-18 12:29:44 +0100

    Jad

--- cpp04/ex01/cat.hpp
+++ cpp04/ex01/cat.hpp
@@ -6,16 +6,19 @@
 /*   By: cjad <cjad@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 19:00:20 by cjad              #+#    #+#             */
-/*   Updated: 2022/06/17 19:06:59 by cjad             ###   ########.fr       */
+/*   Updated: 2022/06/18 12:27:27 by cjad             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CAT_HPP
 # define CAT_HPP
 #include "animal.hpp"
+#include "brain.hpp"
 
 class cat : public animal
 {
+private:
+	brain* brain;
 public:
 	cat();
 	void makeSound() const;

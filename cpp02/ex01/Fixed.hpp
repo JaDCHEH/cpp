37e43e38b4e1fac6c08f890bf8a250fa77d2d012
Jad Chehboune <cjad@e1r10p10.1337.ma>
2022-08-06 17:44:45 +0100

    update general

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: cjad <cjad@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 18:34:00 by cjad              #+#    #+#             */
-/*   Updated: 2022/07/29 16:57:58 by cjad             ###   ########.fr       */
+/*   Updated: 2022/08/06 17:41:28 by cjad             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,7 @@ public:
 	int toInt( void ) const;
 	~Fixed();
 };
+
+std::ostream& operator<<(std::ostream& os, const Fixed &fixed);
 
 #endif
--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: cjad <cjad@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 18:34:00 by cjad              #+#    #+#             */
-/*   Updated: 2022/07/29 16:58:05 by cjad             ###   ########.fr       */
+/*   Updated: 2022/08/06 17:41:39 by cjad             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,7 @@ public:
 	static const Fixed& min(const Fixed &n1, const Fixed &n2);
 	~Fixed();
 };
+
+std::ostream& operator<<(std::ostream& os, const Fixed &fixed);
 
 #endif

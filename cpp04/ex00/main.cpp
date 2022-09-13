0a136f74be751fbfa5fe06fcf164a8c631b326e9
Jad Chehboune <cjad@e1r10p8.1337.ma>
2022-09-13 16:26:41 +0100

    update general on cpp04

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,22 +6,42 @@
 /*   By: cjad <cjad@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 19:05:29 by cjad              #+#    #+#             */
-/*   Updated: 2022/09/12 16:17:28 by cjad             ###   ########.fr       */
+/*   Updated: 2022/09/13 16:25:03 by cjad             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 #include "Dog.hpp"
-
+#include "WrongCat.hpp"
 int main()
 {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound();
-	j->makeSound();
-	meta->makeSound();
-	return 0;
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << "The animal type is : " << j->getType() << " " << std::endl;
+    std::cout << "The animal type is : " << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
+    
+    delete meta;
+    delete j;
+    delete i;
+    return (0);
 }
+
+
+// int main()
+// {
+//     const Animal* meta = new Animal();
+//     const Animal* j = new Dog();
+//     const WrongAnimal* i = new WrongCat();
+//     std::cout << j->getType() << " " << std::endl;
+//     std::cout << i->getType() << " " << std::endl;
+//     i->makeSound(); //will output the cat sound!
+//     j->makeSound();
+//     meta->makeSound();
+//     delete meta;
+//     delete j;
+//     return (0);
+// }
--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,18 +6,29 @@
 /*   By: cjad <cjad@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 19:05:29 by cjad              #+#    #+#             */
-/*   Updated: 2022/09/11 16:16:47 by cjad             ###   ########.fr       */
+/*   Updated: 2022/09/13 16:26:18 by cjad             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "Brain.hpp"
 
 int main()
 {
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	delete j;
-	delete i;
-	return 0;
+const Animal* j = new Dog();
+const Animal* i = new Cat();
+delete j;//should not create a leak
+delete i;
+Animal *animal[10];
+    for(int i = 0; i < 5; i++)
+        animal[i] = new Dog();
+        
+    for(int i = 5; i < 10; i++)
+        animal[i] = new Cat();
+    
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << animal[i]->getType() << std::endl;
+    }
 }
